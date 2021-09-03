a0905f08aba2e1dea0fa590203080bec82581cb5
Nivasan <58383171+NivasanV@users.noreply.github.com>
2021-09-03 19:03:30 +0530

    Day 1 Assignment Submitted

--- Day1/FString.py
+++ Day1/FString.py
@@ -1,5 +1,5 @@
 Str = "Python"
 var = 1
-print(f"I prefered {Str} is my {Mark}st Programming Language")
+print(f"I prefered {Str} is my {var}st Programming Language")
 
 # Output I prefered Python is my 1st Programming Language
